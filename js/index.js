d524f1ec9b0b0ab50768fe76d2f92a5284400798
yuriikhomych23 <yuriikhomych94@gmail.com>
2019-10-11 16:41:00 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -53,3 +53,24 @@ function fetchJson() {
             document.querySelector('.account-balance').innerText = clientData.balance;
         } );
 }
+
+
+// Form with backend handler
+document.querySelector('.login-form input[type=submit]')
+    .addEventListener('click', login);
+
+function login(e) {
+    e.preventDefault();
+    fetch('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            name: document.querySelector('.login-form input[name=name]').value,
+            password: document.querySelector('.login-form input[name=password]').value
+        })
+    })
+        .then(_ => document.querySelector('.login-form').reset());
+}
